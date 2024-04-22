8cf6c80e5f297b487494506bb8fa73786e181051
Edu Paz <jooeduardo8@gmail.com>
2024-04-22 18:40:30 -0300

    adiciona exercicios aula 0

---
+++ src/main/java/competec/aula0_tipos_primitivos/Exercicio1.java
@@ -0,0 +1,15 @@
+package competec.aula0_tipos_primitivos;
+
+/*
+1. Crie uma variável que informa a primeira letra do seu nome;
+2. Crie uma variável que informa a sua idade;
+3. Crie uma variável informando se você tomou café da manhã;
+*/
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        // Escreva seu código aqui ⬇️
+
+
+    }
+}
---
+++ src/main/java/competec/aula0_tipos_primitivos_gabarito/Exercicio1Resolvido.java
@@ -0,0 +1,25 @@
+package competec.aula0_tipos_primitivos_gabarito;
+
+/*
+1. Crie uma variável que informa a primeira letra do seu nome;
+2. Crie uma variável que informa a sua idade;
+3. Crie uma variável informando se você tomou café da manhã;
+*/
+
+public class Exercicio1Resolvido {
+    public static void main(String[] args) {
+        // valores char devem estar entra aspas simples ''
+        char primeiraLetra = 'M';
+
+        // valores inteiros são representados apenas por números
+        int idade = 24;
+
+        // booleanos podem receber true (verdadeiro) ou false (falso)
+        boolean tomouCafe = true;
+
+        // ainda temos os tipos numérico com ponto flutuante
+        // valores float devem terminar com a letra f (por isso o uso de double é mais recomendado)
+        float precoBanana = 3.5f;
+        double pesoEmKg = 65.8;
+    }
+}
